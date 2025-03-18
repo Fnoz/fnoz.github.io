05851e426a4adeccb672aa338f3f52c3879a3b86
Fnoz <fnoz@qq.com>
2025-03-18 23:42:20 +0800

    1. Icon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "INII.tech",
   description: "INII is a technology company dedicated to developing high-quality iOS and macOS applications, including ColorCard, iFrame, Progress, and Menu AI.",
-  icons: [
-    {
-      rel: 'icon',
-      url: '/icon.png',
-    },
-  ],
   manifest: '/manifest.json'
 };
 
