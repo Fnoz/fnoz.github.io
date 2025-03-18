6dbc84b6ce77716fe45b1df8c5b22f27c502f695
Fnoz <fnoz@qq.com>
2025-03-19 00:42:50 +0800

    Revert "Configure GitHub Pages deployment"
    This reverts commit 756886ba5d9460b0e1f553ea03b0e664c14d70f1.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  // 启用静态导出
-  images: {
-    unoptimized: true, // GitHub Pages 不支持 Next.js 的图片优化功能
-  },
-  basePath: '/inii.tech', // 仓库名作为基础路径
+  /* config options here */
 };
 
 export default nextConfig;
