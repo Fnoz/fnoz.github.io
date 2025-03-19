320f64b1e78df7e0037d30defb302cd00383e7fd
Fnoz <fnoz@qq.com>
2025-03-19 23:47:48 +0800

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/inii.tech',
+  assetPrefix: '/inii.tech',
   /* config options here */
 };
 
