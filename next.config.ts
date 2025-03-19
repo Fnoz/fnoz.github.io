8a43a8585252e52d48a784b539fab0fb3af03590
Fnoz <fnoz@qq.com>
2025-03-20 00:45:51 +0800

    Revert "Configure GitHub Pages deployment"
    This reverts commit 320f64b1e78df7e0037d30defb302cd00383e7fd.

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/inii.tech',
-  assetPrefix: '/inii.tech',
   /* config options here */
 };
 
